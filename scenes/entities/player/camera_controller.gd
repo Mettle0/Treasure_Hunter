adcb1f7e21a7268f1cc9030a73fb41411a45fe48
gwyn-b <gwynb94@outlook.com>
2025-06-01 19:48:15 -0500

    Updated Camera
    -Mouse controls camera
    -Implemented capture and release of mouse cursor

--- scenes/entities/player/camera_controller.gd
+++ scenes/entities/player/camera_controller.gd
@@ -6,16 +6,27 @@ extends Node3D
 @export var vertical_acceleration := 2.0
 @export var mouse_accesleration := 0.005
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _process(delta: float) -> void:
 	var joy_dir = Input.get_vector("pan_left","pan_right","pan_up","pan_down")
 	rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
 
-#func _input(event: InputEvent) -> void:
-	#if event is InputEventMouseMotion:
-		#rotate_from_vector(event.relative * mouse_accesleration)
+func _input(event: InputEvent) -> void:
+	capture_mouse(event)
+	if event is InputEventMouseMotion:
+		rotate_from_vector(event.relative * mouse_accesleration)
 
 func rotate_from_vector(v: Vector2):
 	if v.length() == 0: return
 	rotation.y -= v.x
 	rotation.x -= v.y
 	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
+
+func capture_mouse(event) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event.is_action_pressed("left click"):
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
