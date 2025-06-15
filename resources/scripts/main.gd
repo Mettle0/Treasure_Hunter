d286183edfa3b50020531915f584bdbf84a1c3bc
gwyn-b <gwynb94@outlook.com>
2025-06-15 00:30:46 -0500

    Debug Radar
    -Added debug radar that prints the magnitude of the distance vector between the player and the chosen spawn positions. This information can be used for a proximity detection system

--- resources/scripts/main.gd
+++ resources/scripts/main.gd
@@ -1,1 +1,34 @@
 extends Node
+
+#Get references to all children of main
+@onready var ui: CanvasLayer = $UI
+@onready var player: CharacterBody3D = $Player
+@onready var world: Node3D = $World
+@onready var radar_distance_calc: Timer = $"Global Timers/RadarDistanceCalc"
+
+@onready var current_item_locations: Array
+@onready var radarDistance: Array
+
+
+
+
+
+
+
+func _ready() -> void:
+	EventBus.item_positions.connect(_item_positions_received)
+	
+	
+	
+func _item_positions_received(pos) -> void:
+	current_item_locations = pos
+	print(current_item_locations)
+
+
+func _on_radar_distance_calc_timeout() -> void:
+	radarDistance.clear()
+	for loc in current_item_locations:
+		var distanceVector: Vector3 = loc - player.position
+		var distanceMag = distanceVector.length()
+		radarDistance.append(distanceMag)
+	print(radarDistance)
--- scenes/utilities/location_manager.gd
+++ scenes/utilities/location_manager.gd
@@ -2,25 +2,38 @@ extends Node3D
 
 @onready var chosen_locations: Array
 @onready var locations: Array
+
+@export var NumberOfItems: int
 const TREASURE = preload("res://scenes/objects/treasure.tscn")
 
 
 func _ready() -> void:
-	#Todo: Add comments
+	#Connect to treasure collection signal
 	EventBus.treasure_collected.connect(_on_treasure_collected)
 	
+	#Get an array of all possible locations, which exist as nodes that are
+	#added as children of the location manager
 	locations = get_children()
-	var treasure_set: Array
-	treasure_set = get_locations()
-	for loc in range(len(treasure_set)):
-		var newTreasure = TREASURE.instantiate()
-		treasure_set[loc].add_child(newTreasure)
-
-func get_locations() -> Array:
+	
+	var treasure_set = get_spawns()					#Run get spawns function, and put them in an array.
+	for loc in range(len(treasure_set)):			#These are the chosen spawn points for the level.
+		var newTreasure = TREASURE.instantiate()	#Loops through the length of the array of chosen
+		treasure_set[loc].add_child(newTreasure)	#spawn points, for any arbitrary number of items.
+	
+	var spawn_locations: Array
+	for spawn in treasure_set:
+		spawn_locations.append(spawn.position)
+	EventBus.item_positions.emit(spawn_locations)
+	
+	
+func get_spawns() -> Array:
+	#get_spawns()	First, clears chosen locations array. Then, creates a duplicate of the locations
+	#array that can be freely shuffled. Then, uses slice on this shuffled array to make a new array of
+	#shuffled spawn points depending on the NumberOfItems variable. Returns an array.
 	chosen_locations.clear()
 	var choices = locations.duplicate()
 	choices.shuffle()
-	chosen_locations = choices.slice(0, 3)
+	chosen_locations = choices.slice(0, NumberOfItems)
 	return chosen_locations
 
 func spawn_treasure() -> void:
